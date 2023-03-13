125609829d6be49f536df522dc4d54b097c0783c
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-03-13 10:39:31 -0700

    Create item.cs

---
+++ item.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace api.Model
+{
+    public class Author 
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        //Navigation properties
+        [JsonIgnore]
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
